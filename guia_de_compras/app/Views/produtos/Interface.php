0aa967842d9b5179c13018474d1342df1a8c9cb9
maiconmosena <maiconmosena@gmail.com>
2023-11-08 00:21:09 -0300

    hoje

---
+++ guia_de_compras/app/Views/produtos/Interface.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interface</title>
+</head>
+<body>
+    <!--interface de direcionamento
+    seria melhor criar um botao?-->
+   <div id="inteface">
+        <table id="tabela">
+            <tr><td class="ce"><a href="#"></a>Cozinha</td></tr>
+            <tr><td class="ce"><a href="#"></a>WC</td></tr>
+            <tr><td class="cd"><a href="#"></a>Adicionar</td></tr>
+            <tr><td class="ce"><a href="#"></a>Lavaderia</td></tr>
+            <tr><td class="cd"><a href="#"></a>Listar</td></tr>
+            <tr><td class="ce"><a href="#"></a>Geladeira</td></tr>
+            <tr><td class="cd"><a href="#"></a>Comprar</td></tr>
+            <tr><td class="ce"><a href="#"></a>Estante</td></tr>            
+        </table>
+   </div>
+</body>
+</html>
--- guia_de_compras/app/Views/produtos/Login.php
+++ guia_de_compras/app/Views/produtos/Login.php
@@ -1,6 +1,19 @@
-<!--aqui vai para a area de add, listar, comprar
-    ambos podem ser feitos na mesma view
+  <!--tratamento de erros-->
+<?php if(session()->getFlashdata('error')): ?>
+    <div class="alert alert-danger"><?= session()->getFlashdata('error') ?></div>
+<?php endif;?>
 
-1 criar o login
-2 rever a aula 1 crud
-3 ajustar as rotas-->
+<?php if(session()->getFlashdata('success')): ?>
+    <div class="alert alert-success"><?= session()->getFlashdata('success') ?></div>
+<?php endif;?>
+
+  <!--furmulario de login do usuario-->
+<form action="#" method="post" target="_self">
+  <label>Nome:</label><br>
+  <input type="text" id="senha" name="senha" value="..." required ><br>
+  <label>Senha:</label><br>
+  <input type="password" id="password" name="password" value="..." required ><br>  
+  <input type="submit" value="Login">
+</form> 
+
+<a href = "#">#</a><!--aguardar definição para enxugar codigo-->
